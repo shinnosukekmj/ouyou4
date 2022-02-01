9ef9805627dd477e1e21e60aaa0cd1e1b7522ae4
EC2 Default User <ec2-user@ip-172-31-39-48.ec2.internal>
2022-02-01 05:45:41 +0000

    yes

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many:books,dependent: :destroy
+  has_many:favorites,dependent: :destroy
+  has_many:book_comments,dependent: :destroy
+  has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: {maximum: 50}
+
+  has_many :relationships,foreign_key: :following_id
+  has_many :followings,through: :relationships,source: :follower
+  
+  has_many :reverse_of_relationships,class_name: "Relationship",foreign_key: :follower_id
+  has_many :followers,through: :reverse_of_relationships,source: :following
+  
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).present?
+  end
+  
+  def get_profile_image(size)
+    unless profile_image.attached?
+    file_path=Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path),filename: 'default_image.jpg',content_type: "image/jpeg")
+    end
+  profile_image.variant(resize: size).processed
+  end
+end
